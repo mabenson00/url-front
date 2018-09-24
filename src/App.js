8984f6273030f8d989cb7448660de95c67e63ef2
Michael Benson <michael@viahero.com>
2018-09-24 14:02:14 -0400

    show new url

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ class App extends Component {
     return (
       <div className="App">
       <UrlForm
-
         />
       </div>
     );
--- src/urlform.js
+++ src/urlform.js
@@ -1,56 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import axios from 'axios'
 
 class UrlForm extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
-      url: ''
+      url: '',
+      new_url: '',
+      submtited: false
 
     }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   };
 
-  handleChange(event) {
-    this.setState({url: event.target.value});
+  handleChange (event) {
+    this.setState({url: event.target.value})
   }
 
-  handleSubmit(event) {
-   event.preventDefault();
-
-   axios.post('http://www.localhost:3000/links',
-            {
-
-                  link: {
-                      long: this.state.url
-                  }
-
-            }
-        );
+  handleSubmit (event) {
+    event.preventDefault()
+    var self = this
+    axios.post('http://www.localhost:3000/links',
+      {
+        link: {
+          long: this.state.url
+        }
       }
+        ).then(function (response) {
+          self.setState({submitted: true, new_url: response.data.short})
+        })
+  }
 
+  render () {
+    return (
+      <div className='url-form'>
+        {!this.state.submitted &&
+          <form onSubmit={this.handleSubmit}>
+            <label>
+             Url to be Shortened:
+             <input type='text' value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type='submit' value='Submit' />
+          </form>
+         }
+        {this.state.submitted &&
+          <div>
+           New URL: http://www.localhost:3000/{this.state.new_url}
+          </div>
+         }
+      </div>
 
-
-    render() {
-
-
-      return (
-        <div className = "url-form">
-        <form onSubmit={this.handleSubmit}>
-         <label>
-           Url to be Shortened:
-           <input type="text" value={this.state.value} onChange={this.handleChange} />
-         </label>
-         <input type="submit" value="Submit" />
-       </form>
-        </div>
-
-
-
-
-      );
-
+    )
   }
 }
 export default UrlForm
